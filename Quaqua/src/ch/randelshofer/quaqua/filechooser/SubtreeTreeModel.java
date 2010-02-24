e877da579e49a309b1f4ce73bfa37a01e6a4bece
wrandelshofer <wrandelshofer@27033605-f84b-e38a-ee46-f9c4a41192e5>
2010-02-24 21:32:47 +0000

    Fix for issue #105: NullPointerException in NullPointerException in SubtreeTreeModel.toFullPath.
    git-svn-id: https://svn.java.net/svn/quaqua~svn/trunk@195 27033605-f84b-e38a-ee46-f9c4a41192e5

--- Quaqua/src/ch/randelshofer/quaqua/filechooser/SubtreeTreeModel.java
+++ Quaqua/src/ch/randelshofer/quaqua/filechooser/SubtreeTreeModel.java
@@ -10,7 +10,6 @@
  * accordance with the license agreement you entered into with  
  * Werner Randelshofer. For details see accompanying license terms. 
  */
-
 package ch.randelshofer.quaqua.filechooser;
 
 import java.beans.*;
@@ -26,71 +25,73 @@ import javax.swing.tree.*;
  * @version $Id$
  */
 public class SubtreeTreeModel implements TreeModel, TreeModelListener {
+
     /** We store all our listeners here. */
     protected EventListenerList listenerList = new EventListenerList();
     protected PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
-    
     private TreeModel target;
     private Object subtreeRoot;
     private Object[] rootPath;
-    
+
     /** Creates a new instance. */
     public SubtreeTreeModel(TreeModel target) {
         this.target = target;
         this.subtreeRoot = target.getRoot();
-        this.rootPath = new Object[] {subtreeRoot};
+        this.rootPath = new Object[]{subtreeRoot};
         target.addTreeModelListener(this);
     }
-    
+
     public void setPathToRoot(TreePath newValue) {
         setPathToRoot(newValue.getPath());
     }
+
     public void setPathToRoot(Object[] newValue) {
         Object[] oldValue = this.rootPath;
-        if (! Arrays.equals(oldValue, newValue)) {
+        if (!Arrays.equals(oldValue, newValue)) {
             this.rootPath = newValue;
             this.subtreeRoot = newValue[newValue.length - 1];
             changeSupport.firePropertyChange("rootPath", oldValue, newValue);
-            fireTreeStructureChanged(subtreeRoot, new Object[] {rootPath[rootPath.length - 1]});
+            fireTreeStructureChanged(subtreeRoot, new Object[]{rootPath[rootPath.length - 1]});
         }
     }
-    
+
     public TreePath getPathToRoot() {
         return new TreePath(rootPath);
     }
-    
+
     public Object getChild(Object parent, int index) {
         return target.getChild(parent, index);
     }
-    
+
     public int getChildCount(Object parent) {
         return target.getChildCount(parent);
     }
-    
+
     public int getIndexOfChild(Object parent, Object child) {
         return target.getIndexOfChild(parent, child);
     }
-    
+
     public Object getRoot() {
         return subtreeRoot;
     }
-    
+
     public boolean isLeaf(Object node) {
         return target.isLeaf(node);
     }
-    
+
     public TreePath toFullPath(TreePath subtreePath) {
-        return new TreePath(toFullPath(subtreePath.getPath()));
+        return subtreePath == null ? new TreePath(rootPath) : new TreePath(toFullPath(subtreePath.getPath()));
     }
+
     public Object[] toFullPath(Object[] subtreePath) {
         /*
         if (subtreeRoot != target.getRoot()) {
-            Object[] components = new Object[rootPath.length + subtreePath.length - 1];
-            System.arraycopy(rootPath, 0, components, 0, rootPath.length);
-            System.arraycopy(subtreePath, 1, components, rootPath.length, subtreePath.length - 1);
-            return components;
+        Object[] components = new Object[rootPath.length + subtreePath.length - 1];
+        System.arraycopy(rootPath, 0, components, 0, rootPath.length);
+        System.arraycopy(subtreePath, 1, components, rootPath.length, subtreePath.length - 1);
+        return components;
         } else {
-            return subtreePath;
+        return subtreePath;
         }*/
         LinkedList list = new LinkedList(Arrays.asList(subtreePath));
         if (subtreePath.length > 0) {
@@ -101,15 +102,14 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
         }
         return list.toArray();
     }
-    
+
     public void valueForPathChanged(TreePath path, Object newValue) {
-        target.valueForPathChanged(toFullPath(path),newValue);
+        target.valueForPathChanged(toFullPath(path), newValue);
     }
-    
+
     //
     //  Events
     //
-    
     /**
      * Adds a listener for the TreeModelEvent posted after the tree changes.
      *
@@ -119,7 +119,7 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
     public void addTreeModelListener(TreeModelListener l) {
         listenerList.add(TreeModelListener.class, l);
     }
-    
+
     /**
      * Removes a listener previously added with <B>addTreeModelListener()</B>.
      *
@@ -129,20 +129,21 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
     public void removeTreeModelListener(TreeModelListener l) {
         listenerList.remove(TreeModelListener.class, l);
     }
+
     /**
      * Adds a property change listener.
      */
     public void addPropertyChangeListener(PropertyChangeListener l) {
         changeSupport.addPropertyChangeListener(l);
     }
-    
+
     /**
      * Removes a property change listener.
      */
     public void removePropertyChangeListener(PropertyChangeListener l) {
         changeSupport.removePropertyChangeListener(l);
     }
-    
+
     /**
      * Notifies all listeners that have registered interest for
      * notification on this event type.  The event instance
@@ -156,24 +157,25 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
      * @see EventListenerList
      */
     protected void fireTreeNodesChanged(Object source, Object[] path,
-    int[] childIndices,
-    Object[] children) {
+            int[] childIndices,
+            Object[] children) {
         // Guaranteed to return a non-null array
         Object[] listeners = listenerList.getListenerList();
         TreeModelEvent e = null;
         // Process the listeners last to first, notifying
         // those that are interested in this event
-        for (int i = listeners.length-2; i>=0; i-=2) {
-            if (listeners[i]==TreeModelListener.class) {
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
                 // Lazily create the event:
-                if (e == null)
+                if (e == null) {
                     e = new TreeModelEvent(source, path,
-                    childIndices, children);
-                ((TreeModelListener)listeners[i+1]).treeNodesChanged(e);
+                            childIndices, children);
+                }
+                ((TreeModelListener) listeners[i + 1]).treeNodesChanged(e);
             }
         }
     }
-    
+
     /**
      * Notifies all listeners that have registered interest for
      * notification on this event type.  The event instance
@@ -187,24 +189,25 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
      * @see EventListenerList
      */
     protected void fireTreeNodesInserted(Object source, Object[] path,
-    int[] childIndices,
-    Object[] children) {
+            int[] childIndices,
+            Object[] children) {
         // Guaranteed to return a non-null array
         Object[] listeners = listenerList.getListenerList();
         TreeModelEvent e = null;
         // Process the listeners last to first, notifying
         // those that are interested in this event
-        for (int i = listeners.length-2; i>=0; i-=2) {
-            if (listeners[i]==TreeModelListener.class) {
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
                 // Lazily create the event:
-                if (e == null)
+                if (e == null) {
                     e = new TreeModelEvent(source, path,
-                    childIndices, children);
-                ((TreeModelListener)listeners[i+1]).treeNodesInserted(e);
+                            childIndices, children);
+                }
+                ((TreeModelListener) listeners[i + 1]).treeNodesInserted(e);
             }
         }
     }
-    
+
     /**
      * Notifies all listeners that have registered interest for
      * notification on this event type.  The event instance
@@ -218,24 +221,25 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
      * @see EventListenerList
      */
     protected void fireTreeNodesRemoved(Object source, Object[] path,
-    int[] childIndices,
-    Object[] children) {
+            int[] childIndices,
+            Object[] children) {
         // Guaranteed to return a non-null array
         Object[] listeners = listenerList.getListenerList();
         TreeModelEvent e = null;
         // Process the listeners last to first, notifying
         // those that are interested in this event
-        for (int i = listeners.length-2; i>=0; i-=2) {
-            if (listeners[i]==TreeModelListener.class) {
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
                 // Lazily create the event:
-                if (e == null)
+                if (e == null) {
                     e = new TreeModelEvent(source, path,
-                    childIndices, children);
-                ((TreeModelListener)listeners[i+1]).treeNodesRemoved(e);
+                            childIndices, children);
+                }
+                ((TreeModelListener) listeners[i + 1]).treeNodesRemoved(e);
             }
         }
     }
-    
+
     /**
      * Notifies all listeners that have registered interest for
      * notification on this event type.  The event instance
@@ -252,16 +256,17 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
         TreeModelEvent e = null;
         // Process the listeners last to first, notifying
         // those that are interested in this event
-        for (int i = listeners.length-2; i>=0; i-=2) {
-            if (listeners[i]==TreeModelListener.class) {
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
                 // Lazily create the event:
-                if (e == null)
+                if (e == null) {
                     e = new TreeModelEvent(source, path);
-                ((TreeModelListener)listeners[i+1]).treeStructureChanged(e);
+                }
+                ((TreeModelListener) listeners[i + 1]).treeStructureChanged(e);
             }
         }
     }
-    
+
     /*
      * Notifies all listeners that have registered interest for
      * notification on this event type.  The event instance
@@ -278,22 +283,24 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
         TreeModelEvent e = null;
         // Process the listeners last to first, notifying
         // those that are interested in this event
-        for (int i = listeners.length-2; i>=0; i-=2) {
-            if (listeners[i]==TreeModelListener.class) {
+        for (int i = listeners.length - 2; i >= 0; i -= 2) {
+            if (listeners[i] == TreeModelListener.class) {
                 // Lazily create the event:
-                if (e == null)
+                if (e == null) {
                     e = new TreeModelEvent(source, path);
-                ((TreeModelListener)listeners[i+1]).treeStructureChanged(e);
+                }
+                ((TreeModelListener) listeners[i + 1]).treeStructureChanged(e);
             }
         }
     }
+
     /**
      * Returns true, if the specified path if a descendant
      * path of the path to root of this subtree model.
      */
     public boolean isDescendant(Object[] fullpath) {
         if (fullpath.length >= rootPath.length) {
-            for (int i=0; i < rootPath.length; i++) {
+            for (int i = 0; i < rootPath.length; i++) {
                 if (rootPath[i] != fullpath[i]) {
                     return false;
                 }
@@ -302,6 +309,7 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
         }
         return false;
     }
+
     public Object[] toSubPath(Object[] fullPath) {
         if (isDescendant(fullPath)) {
             Object[] subtreePath = new Object[fullPath.length - rootPath.length + 1];
@@ -311,13 +319,14 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
             return null;
         }
     }
+
     /**
      * Returns true, if the specified path if a descendant
      * path of the path to root of this subtree model.
      */
     public boolean isDescendant(TreePath fullpath) {
         if (fullpath.getPathCount() >= rootPath.length) {
-            for (int i=0; i < rootPath.length; i++) {
+            for (int i = 0; i < rootPath.length; i++) {
                 if (rootPath[i] != fullpath.getPathComponent(i)) {
                     return false;
                 }
@@ -326,26 +335,27 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
         }
         return false;
     }
+
     public TreePath toSubPath(TreePath fullPath) {
         Object[] subPath = toSubPath(fullPath.getPath());
         return (subPath == null) ? null : new TreePath(subPath);
     }
-    
+
     public void treeNodesChanged(TreeModelEvent event) {
         Object[] subtreePath = toSubPath(event.getPath());
         if (subtreePath != null) {
             fireTreeNodesChanged(event.getSource(), subtreePath, event.getChildIndices(), event.getChildren());
         }
     }
-    
+
     public void treeNodesInserted(TreeModelEvent event) {
         Object[] subtreePath = toSubPath(event.getPath());
-        
+
         if (subtreePath != null) {
             fireTreeNodesInserted(event.getSource(), subtreePath, event.getChildIndices(), event.getChildren());
         }
     }
-    
+
     public void treeNodesRemoved(TreeModelEvent event) {
         // XXX - We should check here if the rootPath was removed !!!
         Object[] subtreePath = toSubPath(event.getPath());
@@ -353,7 +363,7 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
             fireTreeNodesRemoved(event.getSource(), subtreePath, event.getChildIndices(), event.getChildren());
         }
     }
-    
+
     public void treeStructureChanged(TreeModelEvent event) {
         // XXX - We should check here if the rootPath still exists !!!
         Object[] subtreePath = toSubPath(event.getPath());
