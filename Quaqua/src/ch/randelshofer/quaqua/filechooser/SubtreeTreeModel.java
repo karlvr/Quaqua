223a8067394fe5a8ab371ff400020fe3d3036bd6
wrandelshofer <wrandelshofer@27033605-f84b-e38a-ee46-f9c4a41192e5>
2010-06-22 11:16:11 +0000

    FileChooserUI: - File Hiding was accidentaly disabled when using the QuaquaFileChooserOnlyLAF. - Debugging info was printed on System.out.
    git-svn-id: https://svn.java.net/svn/quaqua~svn/trunk@238 27033605-f84b-e38a-ee46-f9c4a41192e5

--- Quaqua/src/ch/randelshofer/quaqua/filechooser/SubtreeTreeModel.java
+++ Quaqua/src/ch/randelshofer/quaqua/filechooser/SubtreeTreeModel.java
@@ -45,11 +45,8 @@ public class SubtreeTreeModel implements TreeModel, TreeModelListener {
     }
 
     public void setPathToRoot(Object[] newValue) {
-System.out.println("SubtreeTreeModel newpath: "+Arrays.toString(newValue));
-new Throwable().printStackTrace();
         Object[] oldValue = this.rootPath;
         if (!Arrays.equals(oldValue, newValue)) {
-System.out.println("SubtreeTreeModel newpath STORED: "+Arrays.toString(newValue));
             this.rootPath = newValue;
             this.subtreeRoot = newValue[newValue.length - 1];
             changeSupport.firePropertyChange("rootPath", oldValue, newValue);
