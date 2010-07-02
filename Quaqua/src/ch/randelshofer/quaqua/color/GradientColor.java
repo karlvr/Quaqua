0ba61fab5fbeb5eedf237a0b0314c10709b8083c
wrandelshofer <wrandelshofer@27033605-f84b-e38a-ee46-f9c4a41192e5>
2010-07-02 15:47:43 +0000

    JMenu items: - Partial Fix for issue 112: JMenu.setBackground() works now. (JMenuBar.setBackground still does not work).
    git-svn-id: https://svn.java.net/svn/quaqua~svn/trunk@249 27033605-f84b-e38a-ee46-f9c4a41192e5

--- Quaqua/src/ch/randelshofer/quaqua/color/GradientColor.java
+++ Quaqua/src/ch/randelshofer/quaqua/color/GradientColor.java
@@ -10,11 +10,11 @@
  * accordance with the license agreement you entered into with  
  * Werner Randelshofer. For details see accompanying license terms. 
  */
-
 package ch.randelshofer.quaqua.color;
 
 import ch.randelshofer.quaqua.util.*;
 import java.awt.*;
+import javax.swing.plaf.UIResource;
 
 /**
  * GradientColor.
@@ -23,18 +23,24 @@ import java.awt.*;
  * @version $Id$
  */
 public class GradientColor extends PaintableColor {
+
     protected Color color1;
     protected Color color2;
-    
-    public GradientColor(Color c, Color c1, Color c2) {
-        super(c.getRGB(), c.getAlpha() != 255);
-        this.color1 = c1;
-        this.color2 = c2;
+
+    public GradientColor(Color plainColor, Color gradientNorth, Color gradientSouth) {
+        super(plainColor.getRGB(), plainColor.getAlpha() != 255);
+        this.color1 = gradientNorth;
+        this.color2 = gradientSouth;
     }
-    
 
     public Paint getPaint(Component c, int x, int y, int widht, int height) {
-        return new GradientPaint(x,y,color1,x,y+height,color2,true);
+        return new GradientPaint(x, y, color1, x, y + height, color2, true);
     }
 
+    public static class UIResource extends GradientColor implements javax.swing.plaf.UIResource {
+
+        public UIResource(int plainColor, int gradientNorth, int gradientSouth) {
+            super(new Color(plainColor), new Color(gradientNorth), new Color(gradientSouth));
+        }
+    }
 }
