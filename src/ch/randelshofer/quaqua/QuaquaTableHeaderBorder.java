819352344c68c764bf0d84e87ccae0e7940304df
wrandelshofer <wrandelshofer@27033605-f84b-e38a-ee46-f9c4a41192e5>
2010-08-20 17:05:40 +0000

    Paints table headers of sorted columns with blue background.
    git-svn-id: https://svn.java.net/svn/quaqua~svn/trunk/Quaqua@283 27033605-f84b-e38a-ee46-f9c4a41192e5

--- src/ch/randelshofer/quaqua/QuaquaTableHeaderBorder.java
+++ src/ch/randelshofer/quaqua/QuaquaTableHeaderBorder.java
@@ -10,7 +10,6 @@
  * accordance with the license agreement you entered into with  
  * Werner Randelshofer. For details see accompanying license terms. 
  */
-
 package ch.randelshofer.quaqua;
 
 import ch.randelshofer.quaqua.border.BackgroundBorder;
@@ -25,6 +24,7 @@ import javax.swing.border.*;
  * @version $Id$
  */
 public class QuaquaTableHeaderBorder implements BackgroundBorder {
+
     /** Location of the border images. */
     private String imagesLocation;
     private Insets imageInsets;
@@ -32,65 +32,88 @@ public class QuaquaTableHeaderBorder implements BackgroundBorder {
      * This array is created lazily.
      **/
     private Border[] borders;
-    
     /**
      * Column index.
      */
     private int columnIndex = 0;
-    
+    /**
+     * Whether the column is sorted.
+     */
+    private boolean isSorted = false;
+    /**
+     * Whether the column has focus.
+     */
+    private boolean isOnActiveWindow = false;
     private Border tableHeaderBackground = new Border() {
+
         public Insets getBorderInsets(Component c) {
-            return new Insets(0,0,0,0);
+            return new Insets(0, 0, 0, 0);
         }
+
         public boolean isBorderOpaque() {
             return false;
         }
-        
+
         public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
             getBorder(c).paintBorder(c, g, x, y, width, height);
         }
     };
-    
+
     /** Creates a new instance. */
     public QuaquaTableHeaderBorder(String imagesLocation, Insets imageInsets) {
         this.imagesLocation = imagesLocation;
         this.imageInsets = imageInsets;
     }
-    
+
     public Insets getBorderInsets(Component c) {
         return getBorderInsets(c, null);
     }
-    
+
     public Insets getBorderInsets(Component c, Insets insets) {
-        return new Insets(1,2,1,2);
+        return new Insets(1, 2, 1, 2);
     }
-    
+
     public boolean isBorderOpaque() {
         return false;
     }
-    
+
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
     }
-    
+
     public Border getBackgroundBorder() {
         return tableHeaderBackground;
     }
-    
+
     /**
      * FIXME: We should find a better way to pass the column index to the border.
      */
     public void setColumnIndex(int index) {
         this.columnIndex = index;
     }
-    
+
+    /**
+     * FIXME: We should find a better way to pass the sorted state to the border.
+     */
+    public void setSorted(boolean b) {
+        this.isSorted = b;
+    }
+
+    /**
+     * FIXME: We should find a better way to pass the column index to the border.
+     */
+    public void setOnActiveWindow(boolean b) {
+        this.isOnActiveWindow = b;
+    }
+
     private Border getBorder(Component c) {
         if (borders == null) {
             borders = (Border[]) QuaquaBorderFactory.create(imagesLocation, imageInsets, 4, true, true, true);
         }
-        return borders[0];
+        return borders[isSorted && isOnActiveWindow ? 2 : 0];
     }
-    
+
     public static class UIResource extends QuaquaTableHeaderBorder implements javax.swing.plaf.UIResource {
+
         public UIResource(String imagesLocation, Insets imageInsets) {
             super(imagesLocation, imageInsets);
         }
--- src/ch/randelshofer/quaqua/QuaquaTableHeaderUI.java
+++ src/ch/randelshofer/quaqua/QuaquaTableHeaderUI.java
@@ -10,11 +10,16 @@
  * accordance with the license agreement you entered into with  
  * Werner Randelshofer. For details see accompanying license terms. 
  */
-
 package ch.randelshofer.quaqua;
 
 import ch.randelshofer.quaqua.border.BackgroundBorder;
-import java.awt.*;
+import ch.randelshofer.quaqua.util.Methods;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.border.*;
@@ -22,6 +27,7 @@ import javax.swing.plaf.*;
 import javax.swing.plaf.basic.*;
 import javax.swing.table.*;
 import javax.swing.event.*;
+
 /**
  * QuaquaTableHeaderUI.
  *
@@ -30,16 +36,17 @@ import javax.swing.event.*;
  */
 public class QuaquaTableHeaderUI extends BasicTableHeaderUI {
     // Listeners that are attached to the JTable
+
     protected TableColumnModelListener columnModelListener;
-    
+
     /** Creates a new instance. */
     public QuaquaTableHeaderUI() {
     }
-    
+
     public static ComponentUI createUI(JComponent c) {
         return new QuaquaTableHeaderUI();
     }
-    
+
     public void installDefaults() {
         super.installDefaults();
         if (header.getDefaultRenderer() instanceof JLabel) {
@@ -47,7 +54,7 @@ public class QuaquaTableHeaderUI extends BasicTableHeaderUI {
             //((JLabel) header.getDefaultRenderer()).setOpaque(false);
         }
     }
-    
+
     private int viewIndexForColumn(TableColumn aColumn) {
         TableColumnModel cm = header.getColumnModel();
         for (int column = 0; column < cm.getColumnCount(); column++) {
@@ -60,203 +67,117 @@ public class QuaquaTableHeaderUI extends BasicTableHeaderUI {
     //
     // Paint Methods and support
     //
-    
+
     public void paint(Graphics g, JComponent c) {
-	if (header.getColumnModel().getColumnCount() <= 0) { 
-	    return; 
-	}
+        if (header.getColumnModel().getColumnCount() <= 0) {
+            return;
+        }
         boolean ltr = header.getComponentOrientation().isLeftToRight();
 
-	Rectangle clip = g.getClipBounds(); 
+        Rectangle clip = g.getClipBounds();
         Point left = clip.getLocation();
-        Point right = new Point( clip.x + clip.width - 1, clip.y );
-	TableColumnModel cm = header.getColumnModel(); 
-        int cMin = header.columnAtPoint( ltr ? left : right );
-        int cMax = header.columnAtPoint( ltr ? right : left );
+        Point right = new Point(clip.x + clip.width - 1, clip.y);
+        TableColumnModel cm = header.getColumnModel();
+        int cMin = header.columnAtPoint(ltr ? left : right);
+        int cMax = header.columnAtPoint(ltr ? right : left);
         // This should never happen. 
         if (cMin == -1) {
-	    cMin =  0;
+            cMin = 0;
         }
         /*
         // If the table does not have enough columns to fill the view we'll get -1.
         // Replace this with the index of the last column.
         if (cMax == -1) {
-	    cMax = cm.getColumnCount()-1;  
+        cMax = cm.getColumnCount()-1;
         }*/
         // If the table does not have enough columns to fill the view we'll get -1.
         // Replace this with the index of the last column.
         if (cMax == -1) {
-            cMax = cm.getColumnCount()-1;
+            cMax = cm.getColumnCount() - 1;
             Border cellBorder = UIManager.getBorder("TableHeader.cellBorder");
             if (cellBorder instanceof QuaquaTableHeaderBorder) {
-                ((QuaquaTableHeaderBorder) cellBorder).setColumnIndex(-1);
+                QuaquaTableHeaderBorder qthb = ((QuaquaTableHeaderBorder) cellBorder);
+                qthb.setColumnIndex(-1);
             }
             if (cellBorder instanceof BackgroundBorder) {
                 cellBorder = ((BackgroundBorder) cellBorder).getBackgroundBorder();
             }
             if (cellBorder != null) {
-            cellBorder.paintBorder(header,g, cMax, 0, header.getWidth() - cMax, header.getHeight());
+                cellBorder.paintBorder(header, g, cMax, 0, header.getWidth() - cMax, header.getHeight());
             }
         }
 
-	TableColumn draggedColumn = header.getDraggedColumn(); 
-	int columnWidth;
-        Rectangle cellRect = header.getHeaderRect(ltr ? cMin : cMax); 
-	TableColumn aColumn;
-	if (ltr) {
-	    for(int column = cMin; column <= cMax ; column++) { 
-		aColumn = cm.getColumn(column); 
-		columnWidth = aColumn.getWidth();
-		cellRect.width = columnWidth;
-		if (aColumn != draggedColumn) {
-		    paintCell(g, cellRect, column);
-		} 
-		cellRect.x += columnWidth;
-	    }
-	} else {
-	    for(int column = cMax; column >= cMin; column--) {
-		aColumn = cm.getColumn(column);
-		columnWidth = aColumn.getWidth();
-		cellRect.width = columnWidth;
-		if (aColumn != draggedColumn) {
-		    paintCell(g, cellRect, column);
-		}
-                cellRect.x += columnWidth;
-	    }
-	} 
-
-        // Paint the dragged column if we are dragging. 
-        if (draggedColumn != null) { 
-            int draggedColumnIndex = viewIndexForColumn(draggedColumn); 
-	    Rectangle draggedCellRect = header.getHeaderRect(draggedColumnIndex); 
-            
-            // Draw a gray well in place of the moving column. 
-            g.setColor(header.getParent().getBackground());
-            g.fillRect(draggedCellRect.x, draggedCellRect.y,
-                               draggedCellRect.width, draggedCellRect.height);
-
-            draggedCellRect.x += header.getDraggedDistance();
-
-	    // Fill the background. 
-	    g.setColor(header.getBackground());
-	    g.fillRect(draggedCellRect.x, draggedCellRect.y,
-		       draggedCellRect.width, draggedCellRect.height);
- 
-            paintCell(g, draggedCellRect, draggedColumnIndex);
-        }
-
-	// Remove all components in the rendererPane. 
-	rendererPane.removeAll(); 
-    }
-    public void paintOLD(Graphics g, JComponent c) {
-        if (header.getColumnModel().getColumnCount() <= 0) {
-            return;
-        }
-        boolean ltr = header.getComponentOrientation().isLeftToRight();
-        
-        Rectangle clip = g.getClipBounds();
-        Point left = clip.getLocation();
-        Point right = new Point( clip.x + clip.width - 1, clip.y );
-        TableColumnModel cm = header.getColumnModel();
-        int cMin = header.columnAtPoint( ltr ? left : right );
-        int cMax = header.columnAtPoint( ltr ? right : left );
-        // This should never happen.
-        if (cMin == -1) {
-            cMin =  0;
-        }
-        // If the table does not have enough columns to fill the view we'll get -1.
-        // Replace this with the index of the last column.
-        if (cMax == -1) {
-            cMax = cm.getColumnCount()-1;
-            Border cellBorder = UIManager.getBorder("TableHeader.cellBorder");
-            if (cellBorder instanceof QuaquaTableHeaderBorder) {
-                ((QuaquaTableHeaderBorder) cellBorder).setColumnIndex(-1);
-            }
-            if (cellBorder instanceof BackgroundBorder) {
-                cellBorder = ((BackgroundBorder) cellBorder).getBackgroundBorder();
-            }
-            if (cellBorder != null) {
-            cellBorder.paintBorder(header,g, cMax, 0, header.getWidth() - cMax, header.getHeight());
-            }
-        }
-        
         TableColumn draggedColumn = header.getDraggedColumn();
         int columnWidth;
-        int columnMargin = cm.getColumnMargin();
-        Rectangle cellRect = header.getHeaderRect(cMin);
+        Rectangle cellRect = header.getHeaderRect(ltr ? cMin : cMax);
         TableColumn aColumn;
         if (ltr) {
-            for(int column = cMin; column <= cMax ; column++) {
+            for (int column = cMin; column <= cMax; column++) {
                 aColumn = cm.getColumn(column);
                 columnWidth = aColumn.getWidth();
-                cellRect.width = columnWidth - columnMargin;
+                cellRect.width = columnWidth;
                 if (aColumn != draggedColumn) {
                     paintCell(g, cellRect, column);
                 }
                 cellRect.x += columnWidth;
             }
         } else {
-            aColumn = cm.getColumn(cMin);
-            if (aColumn != draggedColumn) {
-                columnWidth = aColumn.getWidth();
-                cellRect.width = columnWidth - columnMargin;
-                cellRect.x += columnMargin;
-                paintCell(g, cellRect, cMin);
-            }
-            for(int column = cMin+1; column <= cMax; column++) {
+            for (int column = cMax; column >= cMin; column--) {
                 aColumn = cm.getColumn(column);
                 columnWidth = aColumn.getWidth();
-                cellRect.width = columnWidth - columnMargin;
-                cellRect.x -= columnWidth;
+                cellRect.width = columnWidth;
                 if (aColumn != draggedColumn) {
                     paintCell(g, cellRect, column);
                 }
+                cellRect.x += columnWidth;
             }
         }
-        
-        // Paint the dragged column if we are dragging.
+
+        // Paint the dragged column if we are dragging. 
         if (draggedColumn != null) {
             int draggedColumnIndex = viewIndexForColumn(draggedColumn);
             Rectangle draggedCellRect = header.getHeaderRect(draggedColumnIndex);
-            
-            // Draw a gray well in place of the moving column.
+
+            // Draw a gray well in place of the moving column. 
             g.setColor(header.getParent().getBackground());
             g.fillRect(draggedCellRect.x, draggedCellRect.y,
-            draggedCellRect.width, draggedCellRect.height);
-            
+                    draggedCellRect.width, draggedCellRect.height);
+
             draggedCellRect.x += header.getDraggedDistance();
-            
+
             // Fill the background.
             g.setColor(header.getBackground());
             g.fillRect(draggedCellRect.x, draggedCellRect.y,
-            draggedCellRect.width, draggedCellRect.height);
-            
+                    draggedCellRect.width, draggedCellRect.height);
+
             paintCell(g, draggedCellRect, draggedColumnIndex);
         }
-        
+
         // Remove all components in the rendererPane.
         rendererPane.removeAll();
     }
+
     /**
      * Attaches listeners to the JTableHeader.
      */
     protected void installListeners() {
         columnModelListener = createColumnModelListener();
-        
+
         header.getColumnModel().addColumnModelListener(columnModelListener);
         super.installListeners();
     }
+
     protected void uninstallListeners() {
         super.uninstallListeners();
         header.getColumnModel().removeColumnModelListener(columnModelListener);
-        
+
         columnModelListener = null;
     }
-    
+
     private TableColumnModelListener createColumnModelListener() {
         return new ColumnHandler();
     }
-    
+
     private Component getHeaderRenderer(int columnIndex) {
         TableColumn aColumn = header.getColumnModel().getColumn(columnIndex);
         TableCellRenderer renderer = aColumn.getHeaderRenderer();
@@ -264,58 +185,74 @@ public class QuaquaTableHeaderUI extends BasicTableHeaderUI {
             renderer = header.getDefaultRenderer();
         }
         return renderer.getTableCellRendererComponent(header.getTable(),
-        aColumn.getHeaderValue(), false, false,
-        -1, columnIndex);
+                aColumn.getHeaderValue(), false, false,
+                -1, columnIndex);
     }
-    
+
     private void paintCell(Graphics g, Rectangle cellRect, int columnIndex) {
         Component component = getHeaderRenderer(columnIndex);
-        
-        if (component instanceof JComponent) {
-            if (((JComponent) component).getBorder() instanceof QuaquaTableHeaderBorder) {
-                QuaquaTableHeaderBorder thb = (QuaquaTableHeaderBorder) ((JComponent) component).getBorder();
-                thb.setColumnIndex(columnIndex);
-            }
+
+        if ((component instanceof JComponent)
+                && (((JComponent) component).getBorder() instanceof QuaquaTableHeaderBorder)) {
+            QuaquaTableHeaderBorder thb = (QuaquaTableHeaderBorder) ((JComponent) component).getBorder();
+            thb.setColumnIndex(columnIndex);
+
+            // Table sorting is only supported since J2SE 6.
+            // Use reflection to determine whether the current column is sorted.
+            boolean isSorted = false;
+            Object rowSorter = Methods.invokeGetter(header.getTable(), "getRowSorter", null);
+            List sortKeys = (List) ((rowSorter == null) ? null : Methods.invokeGetter(rowSorter, "getSortKeys", null));
+            Object sortKey = (sortKeys == null || sortKeys.size() == 0) ? null : sortKeys.get(0);
+            int sortedColumnIndex = (sortKey == null) ? -1 : Methods.invokeGetter(sortKey, "getColumn", -1);
+            isSorted = sortedColumnIndex == columnIndex;
+
+            thb.setSorted(isSorted);
+            thb.setOnActiveWindow(QuaquaUtilities.isOnActiveWindow(header.getTable()));
+            rendererPane.paintComponent(g, component, header, cellRect.x, cellRect.y,
+                    cellRect.width, cellRect.height, true);
+            thb.setSorted(false);
+            thb.setOnActiveWindow(true);
+        } else {
+            rendererPane.paintComponent(g, component, header, cellRect.x, cellRect.y,
+                    cellRect.width, cellRect.height, true);
         }
-        
-        rendererPane.paintComponent(g, component, header, cellRect.x, cellRect.y,
-        cellRect.width, cellRect.height, true);
     }
-    
+
     private class ColumnHandler implements TableColumnModelListener {
+
         private void updateViewport() {
             JTable table = header.getTable();
             Object property = (table == null) ? null : table.getClientProperty("Quaqua.Table.style");
             if (property != null && property.equals("striped")
-            && table.getParent() instanceof JViewport) {
+                    && table.getParent() instanceof JViewport) {
                 JViewport viewport = (JViewport) table.getParent();
                 if (viewport.getHeight() > table.getHeight()) {
                     viewport.repaint(0, table.getHeight(), viewport.getWidth(), viewport.getHeight() - table.getHeight());
                 }
             }
         }
-        
+
         public void columnAdded(TableColumnModelEvent e) {
             updateViewport();
         }
-        
+
         public void columnMarginChanged(ChangeEvent e) {
             updateViewport();
         }
-        
+
         public void columnMoved(TableColumnModelEvent e) {
             updateViewport();
         }
-        
+
         public void columnRemoved(TableColumnModelEvent e) {
             updateViewport();
         }
-        
+
         public void columnSelectionChanged(ListSelectionEvent e) {
             updateViewport();
         }
-        
     }
+
     /**
      * Return the preferred size of the header. The preferred height is the 
      * maximum of the preferred heights of all of the components provided 
@@ -326,57 +263,59 @@ public class QuaquaTableHeaderUI extends BasicTableHeaderUI {
         long width = 0;
         Enumeration enumeration = header.getColumnModel().getColumns();
         while (enumeration.hasMoreElements()) {
-            TableColumn aColumn = (TableColumn)enumeration.nextElement();
+            TableColumn aColumn = (TableColumn) enumeration.nextElement();
             width = width + aColumn.getPreferredWidth();
         }
         return createHeaderSize(width);
     }
+
     private Dimension createHeaderSize(long width) {
         TableColumnModel columnModel = header.getColumnModel();
         // None of the callers include the intercell spacing, do it here.
         if (width > Integer.MAX_VALUE) {
             width = Integer.MAX_VALUE;
         }
-        return new Dimension((int)width, getHeaderHeight());
+        return new Dimension((int) width, getHeaderHeight());
     }
+
     private int getHeaderHeight() {
-        int height = 0; 
-        
+        int height = 0;
+
         int emptyHeight = 0;
-        
-	boolean accomodatedDefault = false; 
+
+        boolean accomodatedDefault = false;
         TableColumnModel columnModel = header.getColumnModel();
-        for(int column = 0; column < columnModel.getColumnCount(); column++) { 
-	    TableColumn aColumn = columnModel.getColumn(column); 
-	    // Configuring the header renderer to calculate its preferred size is expensive. 
-	    // Optimise this by assuming the default renderer always has the same height. 
-	    if (aColumn.getHeaderRenderer() != null || !accomodatedDefault) { 
-		Component comp = getHeaderRenderer(column); 
-		int rendererHeight = comp.getPreferredSize().height; 
-		height = Math.max(height, rendererHeight); 
-                
+        for (int column = 0; column < columnModel.getColumnCount(); column++) {
+            TableColumn aColumn = columnModel.getColumn(column);
+            // Configuring the header renderer to calculate its preferred size is expensive.
+            // Optimise this by assuming the default renderer always has the same height.
+            if (aColumn.getHeaderRenderer() != null || !accomodatedDefault) {
+                Component comp = getHeaderRenderer(column);
+                int rendererHeight = comp.getPreferredSize().height;
+                height = Math.max(height, rendererHeight);
+
                 if (comp instanceof JComponent) {
                     Border b = ((JComponent) comp).getBorder();
                     if (b != null && (b instanceof UIResource)) {
                         Insets insets = b.getBorderInsets(comp);
                         emptyHeight = Math.max(emptyHeight, insets.top + insets.bottom);
-                        }
                     }
-                
-                
-		// If the header value is empty (== "") in the 
-		// first column (and this column is set up 
-		// to use the default renderer) we will 
-		// return zero from this routine and the header 
-		// will disappear altogether. Avoiding the calculation 
-		// of the preferred size is such a performance win for 
-		// most applications that we will continue to 
-		// use this cheaper calculation, handling these 
-		// issues as `edge cases'. 
-		if (rendererHeight > emptyHeight) { 
-		   accomodatedDefault = true; 
-		}
-	    }
+                }
+
+
+                // If the header value is empty (== "") in the
+                // first column (and this column is set up
+                // to use the default renderer) we will
+                // return zero from this routine and the header
+                // will disappear altogether. Avoiding the calculation
+                // of the preferred size is such a performance win for
+                // most applications that we will continue to
+                // use this cheaper calculation, handling these
+                // issues as `edge cases'.
+                if (rendererHeight > emptyHeight) {
+                    accomodatedDefault = true;
+                }
+            }
         }
         // If all header cells are empty, we make the border disappear alltogether
         return (height <= emptyHeight) ? 0 : height;
