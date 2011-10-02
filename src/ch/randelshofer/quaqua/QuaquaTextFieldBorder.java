c38dff82f79ededda2df82b40e89369f5711a699
wrandelshofer <wrandelshofer@27033605-f84b-e38a-ee46-f9c4a41192e5>
2011-10-02 11:22:58 +0000

    Fixes QUAQUA-149 "NullPointerException caused by QuaquaTextFieldBorder".
    git-svn-id: https://svn.java.net/svn/quaqua~svn/trunk/Quaqua@446 27033605-f84b-e38a-ee46-f9c4a41192e5

--- src/ch/randelshofer/quaqua/QuaquaTextFieldBorder.java
+++ src/ch/randelshofer/quaqua/QuaquaTextFieldBorder.java
@@ -8,7 +8,6 @@
  * license agreement you entered into with Werner Randelshofer.
  * For details see accompanying license terms.
  */
-
 package ch.randelshofer.quaqua;
 
 import ch.randelshofer.quaqua.border.VisualMarginBorder;
@@ -44,6 +43,7 @@ import javax.swing.border.*;
  * <br>1.0  July 4, 2004  Created.
  */
 public class QuaquaTextFieldBorder extends VisualMarginBorder implements BackgroundBorder {
+
     /** Location of the border images. */
     private String imagesLocation;
     private Insets imageInsets;
@@ -57,57 +57,58 @@ public class QuaquaTextFieldBorder extends VisualMarginBorder implements Backgro
     private Border[] plainBorders;
     private Border[] searchBorders;
     private Border[] smallSearchBorders;
-    
+
     private static class BgBorder implements Border, Serializable {
+
         QuaquaTextFieldBorder outer;
+
         public BgBorder(QuaquaTextFieldBorder outer) {
-            this.outer=outer;
+            this.outer = outer;
         }
+
         public Insets getBorderInsets(Component c) {
-            return new Insets(0,0,0,0);
+            return new Insets(0, 0, 0, 0);
         }
+
         public boolean isBorderOpaque() {
             return false;
         }
-        
+
         public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
             if (c.isOpaque()) {
                 g.setColor(UIManager.getColor("Panel.background"));
                 g.fillRect(0, 0, width, height);
             }
-            
+
             g.setColor(c.getBackground());
-            Insets insets = outer.getVisualMargin(c, new Insets(0,0,0,0));
+            Insets insets = outer.getVisualMargin(c, new Insets(0, 0, 0, 0));
             if (isSearchField(c)) {
                 int arc = Math.min(
                         width - insets.left - insets.right - 8,
                         height - insets.top - insets.bottom - 7);
-                
+
                 g.fillRoundRect(
                         insets.left + 4,
                         insets.bottom + 3,
                         width - insets.left - insets.right - 8,
                         height - insets.top - insets.bottom - 7,
-                        arc, arc
-                        );
+                        arc, arc);
             } else {
                 g.fillRect(
                         insets.left + 3,
                         insets.bottom + 3,
                         width - insets.left - insets.right - 6,
-                        height - insets.top - insets.bottom - 7
-                        );
+                        height - insets.top - insets.bottom - 7);
             }
         }
     };
     private BgBorder textFieldBackground = new BgBorder(this);
-    
-    
+
     /** Creates a new instance. */
     public QuaquaTextFieldBorder(String imagesLocation, Insets imageInsets,
             String searchImagesLocation, Insets searchImageInsets,
             String smallSearchImagesLocation, Insets smallSearchImageInsets) {
-        super(3,3,2,3);
+        super(3, 3, 2, 3);
         this.imageInsets = imageInsets;
         this.searchImageInsets = searchImageInsets;
         this.smallSearchImageInsets = smallSearchImageInsets;
@@ -115,64 +116,67 @@ public class QuaquaTextFieldBorder extends VisualMarginBorder implements Backgro
         this.searchImagesLocation = searchImagesLocation;
         this.smallSearchImagesLocation = smallSearchImagesLocation;
     }
-    
-     private static boolean isSearchField(Component c) {
+
+    private static boolean isSearchField(Component c) {
         if (c instanceof JComponent) {
             JComponent jc = (JComponent) c;
-            return jc.getClientProperty("Quaqua.TextField.style") != null &&
-                    jc.getClientProperty("Quaqua.TextField.style").equals("search");
+            return jc.getClientProperty("Quaqua.TextField.style") != null
+                    && jc.getClientProperty("Quaqua.TextField.style").equals("search");
         }
         return false;
     }
-    
+
     @Override
     public Insets getBorderInsets(Component c, Insets insets) {
         insets = getVisualMargin(c, insets);
         boolean isSmall = QuaquaUtilities.getSizeVariant(c) == QuaquaUtilities.SizeVariant.SMALL;
 
-        Insets inner = isSmall ?
-            (isSearchField(c) ? new Insets(6, 10, 5, 10) : UIManager.getInsets("TextField.borderInsetsSmall")) :
-                (isSearchField(c) ? new Insets(6, 9, 6, 9) : UIManager.getInsets("TextField.borderInsets"));
-        InsetsUtil.addTo(inner, insets);
-        
+        Insets inner = isSmall
+                ? (isSearchField(c) ? new Insets(6, 10, 5, 10) : UIManager.getInsets("TextField.borderInsetsSmall"))
+                : (isSearchField(c) ? new Insets(6, 9, 6, 9) : UIManager.getInsets("TextField.borderInsets"));
+        // BEGIN FIX QUAQUA-149 NPE
+        if (inner != null) {
+            InsetsUtil.addTo(inner, insets);
+        }
+        // END FIX QUAQUA-149 NPE
+
         if (c instanceof JTextComponent) {
             Insets margin = ((JTextComponent) c).getMargin();
             if (margin != null) {
                 InsetsUtil.addTo(margin, insets);
             }
         }
-        
+
         return insets;
     }
-    
+
     @Override
     public boolean isBorderOpaque() {
         return false;
     }
-    
+
     @Override
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-        Insets margin = getVisualMargin(c, new Insets(0,0,0,0));
+        Insets margin = getVisualMargin(c, new Insets(0, 0, 0, 0));
         Border border = getBorder(c);
         border.paintBorder(c, g,
                 x + margin.left, y + margin.top,
                 width - margin.left - margin.right,
-                height - margin.top - margin.bottom
-                );
-        
+                height - margin.top - margin.bottom);
+
         if (c instanceof JTextComponent) {
             Debug.paint(g, (JTextComponent) c, ((JTextComponent) c).getUI());
         }
     }
-    
+
     public Border getBackgroundBorder() {
         return textFieldBackground;
     }
-    
+
     private Border getBorder(Component c) {
         Border[] borders;
         if (isSearchField(c)) {
-                boolean isSmall =           QuaquaUtilities.getSizeVariant(c) ==QuaquaUtilities.SizeVariant.SMALL;
+            boolean isSmall = QuaquaUtilities.getSizeVariant(c) == QuaquaUtilities.SizeVariant.SMALL;
             if (isSmall) {
                 if (smallSearchBorders == null) {
                     smallSearchBorders = (Border[]) QuaquaBorderFactory.create(smallSearchImagesLocation, smallSearchImageInsets, 3, true);
@@ -190,7 +194,7 @@ public class QuaquaTextFieldBorder extends VisualMarginBorder implements Backgro
             }
             borders = plainBorders;
         }
-        
+
         boolean isEditable;
         if (c instanceof JTextComponent) {
             isEditable = ((JTextComponent) c).isEditable();
@@ -206,16 +210,17 @@ public class QuaquaTextFieldBorder extends VisualMarginBorder implements Backgro
             return borders[1];
         }
     }
-    
-    
+
     public static class UIResource extends QuaquaTextFieldBorder implements javax.swing.plaf.UIResource {
-        public UIResource(String imagesLocation, 
+
+        public UIResource(String imagesLocation,
                 String searchImagesLocation,
                 String smallSearchImagesLocation) {
-            this(imagesLocation, new Insets(6,6,5,6), 
-                    searchImagesLocation, new Insets(13,13,13,13),
-                    smallSearchImagesLocation, new Insets(11,13,11,13));
+            this(imagesLocation, new Insets(6, 6, 5, 6),
+                    searchImagesLocation, new Insets(13, 13, 13, 13),
+                    smallSearchImagesLocation, new Insets(11, 13, 11, 13));
         }
+
         public UIResource(String imagesLocation, Insets imageInsets,
                 String searchImagesLocation, Insets searchImageInsets,
                 String smallSearchImagesLocation, Insets smallSearchImageInsets) {
