c279d6b9f9eac9ee4d21b2afd34ccff19a7d6aa3
Karl von Randow <karl@cactuslab.com>
2013-07-31 16:48:11 +1200

    Toolbar borders: fix for color match on Java 1.6 vs 1.7. Allow toolbar title border to use InactivatableColorUIResource.

--- Quaqua/src/ch/randelshofer/quaqua/mavericks/Quaqua16MavericksLookAndFeel.java
+++ Quaqua/src/ch/randelshofer/quaqua/mavericks/Quaqua16MavericksLookAndFeel.java
@@ -1,8 +1,5 @@
 package ch.randelshofer.quaqua.mavericks;
 
-import javax.swing.UIDefaults;
-import javax.swing.plaf.ColorUIResource;
-
 import ch.randelshofer.quaqua.lion.Quaqua16LionLookAndFeel;
 
 public class Quaqua16MavericksLookAndFeel extends Quaqua16LionLookAndFeel {
@@ -13,19 +10,6 @@ public class Quaqua16MavericksLookAndFeel extends Quaqua16LionLookAndFeel {
 
 	protected Quaqua16MavericksLookAndFeel(String className) {
 		super(className);
-	}
-
-	@Override
-	protected void initDesignDefaults(UIDefaults table) {
-		super.initDesignDefaults(table);
-		
-        Object toolBarBackground = new ColorUIResource(222, 222, 222);
-        
-		Object[] uiDefaults = new Object[] {
-			"ToolBar.title.background", toolBarBackground
-		};
-		
-		putDefaults(table, uiDefaults);
 	}
 
 }
