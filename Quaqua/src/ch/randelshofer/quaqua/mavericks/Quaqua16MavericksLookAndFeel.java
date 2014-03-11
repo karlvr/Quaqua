98c740a71320c8e74d6e0dd2dd1b38ba673f7843
Karl von Randow <karl@xk72.com>
2014-03-12 00:26:13 +1300

    Mavericks toolbar background fix for Java 1.6
    Now that the Mavericks LAF is using Mountain Lion from Werner as its superclass.

--- Quaqua/src/ch/randelshofer/quaqua/mavericks/Quaqua16MavericksLookAndFeel.java
+++ Quaqua/src/ch/randelshofer/quaqua/mavericks/Quaqua16MavericksLookAndFeel.java
@@ -1,8 +1,13 @@
 package ch.randelshofer.quaqua.mavericks;
 
-import ch.randelshofer.quaqua.lion.Quaqua16LionLookAndFeel;
+import javax.swing.UIDefaults;
+import javax.swing.plaf.ColorUIResource;
 
-public class Quaqua16MavericksLookAndFeel extends Quaqua16LionLookAndFeel {
+import ch.randelshofer.quaqua.QuaquaManager;
+import ch.randelshofer.quaqua.color.InactivatableColorUIResource;
+import ch.randelshofer.quaqua.mountainlion.Quaqua16MountainLionLookAndFeel;
+
+public class Quaqua16MavericksLookAndFeel extends Quaqua16MountainLionLookAndFeel {
 
 	public Quaqua16MavericksLookAndFeel() {
 		super();
@@ -10,6 +15,30 @@ public class Quaqua16MavericksLookAndFeel extends Quaqua16LionLookAndFeel {
 
 	protected Quaqua16MavericksLookAndFeel(String className) {
 		super(className);
+	}
+
+	@Override
+	protected void initDesignDefaults(UIDefaults table) {
+		super.initDesignDefaults(table);
+		
+		String javaVersion = QuaquaManager.getProperty("java.version", "");
+		if (javaVersion.startsWith("1.5") || javaVersion.startsWith("1.6")) {
+			Object toolBarBackground = table.get("control");
+	        
+			Object[] uiDefaults = new Object[] {
+				"ToolBar.title.background", toolBarBackground
+			};
+			
+			putDefaults(table, uiDefaults);
+		} else {
+	        Object toolBarBackground = new InactivatableColorUIResource(new ColorUIResource(222, 222, 222), new ColorUIResource(246, 246, 246));
+	        
+			Object[] uiDefaults = new Object[] {
+				"ToolBar.title.background", toolBarBackground
+			};
+			
+			putDefaults(table, uiDefaults);
+		}
 	}
 
 }
