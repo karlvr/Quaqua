6b9af0d94abd3bb467edf110ede4f2ee1756f352
David Maplesden <david@maplesden.co.nz>
2015-04-01 11:45:17 +1300

    Support painting drop lines for tables.

--- Quaqua/src/ch/randelshofer/quaqua/QuaquaTableUI.java
+++ Quaqua/src/ch/randelshofer/quaqua/QuaquaTableUI.java
@@ -9,9 +9,11 @@ package ch.randelshofer.quaqua;
 
 import ch.randelshofer.quaqua.color.InactivatableColorUIResource;
 import ch.randelshofer.quaqua.util.ViewportPainter;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.beans.*;
+
 import javax.swing.*;
 import javax.swing.plaf.*;
 import javax.swing.plaf.basic.*;
@@ -125,27 +127,44 @@ public class QuaquaTableUI extends BasicTableUI
     /** Paint a representation of the <code>table</code> instance
      * that was set in installUI().
      */
-    @Override
     public void paint(Graphics g, JComponent c) {
-        if (table.getRowCount() <= 0 || table.getColumnCount() <= 0) {
+        Rectangle clip = g.getClipBounds();
+
+        Rectangle bounds = table.getBounds();
+        // account for the fact that the graphics has already been translated
+        // into the table's bounds
+        bounds.x = bounds.y = 0;
+
+        if (table.getRowCount() <= 0 || table.getColumnCount() <= 0 ||
+                // this check prevents us from painting the entire table
+                // when the clip doesn't intersect our bounds at all
+                !bounds.intersects(clip)) {
+
+            paintDropLines(g);
             return;
         }
-        Rectangle clip = g.getClipBounds();
+
+        boolean ltr = table.getComponentOrientation().isLeftToRight();
+
         Point upperLeft = clip.getLocation();
-        Point lowerRight = new Point(clip.x + clip.width - 1, clip.y + clip.height - 1);
+        Point lowerRight = new Point(clip.x + clip.width - 1,
+                                     clip.y + clip.height - 1);
+
         int rMin = table.rowAtPoint(upperLeft);
         int rMax = table.rowAtPoint(lowerRight);
-        // This should never happen.
+        // This should never happen (as long as our bounds intersect the clip,
+        // which is why we bail above if that is the case).
         if (rMin == -1) {
             rMin = 0;
         }
         // If the table does not have enough rows to fill the view we'll get -1.
-        // Replace this with the row2 of the last row2.
+        // (We could also get -1 if our bounds don't intersect the clip,
+        // which is why we bail above if that is the case).
+        // Replace this with the index of the last row.
         if (rMax == -1) {
-            rMax = table.getRowCount() - 1;
+            rMax = table.getRowCount()-1;
         }
 
-        boolean ltr = table.getComponentOrientation().isLeftToRight();
         int cMin = table.columnAtPoint(ltr ? upperLeft : lowerRight);
         int cMax = table.columnAtPoint(ltr ? lowerRight : upperLeft);
         // This should never happen.
@@ -153,17 +172,147 @@ public class QuaquaTableUI extends BasicTableUI
             cMin = 0;
         }
         // If the table does not have enough columns to fill the view we'll get -1.
-        // Replace this with the row2 of the last column.
+        // Replace this with the index of the last column.
         if (cMax == -1) {
-            cMax = table.getColumnCount() - 1;
+            cMax = table.getColumnCount()-1;
         }
+
+        // Paint the grid.
+        paintGrid(g, rMin, rMax, cMin, cMax);
 
         // Paint the cells.
         paintCells(g, rMin, rMax, cMin, cMax);
-        // Paint the grid.
-        paintGrid(g, rMin, rMax, cMin, cMax);
+
+        paintDropLines(g);
     }
 
+    private void paintDropLines(Graphics g) {
+        JTable.DropLocation loc = table.getDropLocation();
+        if (loc == null) {
+            return;
+        }
+
+        Color color = UIManager.getColor("Table.dropLineColor");
+        Color shortColor = UIManager.getColor("Table.dropLineShortColor");
+        if (color == null && shortColor == null) {
+            return;
+        }
+
+        Rectangle rect;
+
+        rect = getHDropLineRect(loc);
+        if (rect != null) {
+            int x = rect.x;
+            int w = rect.width;
+            if (color != null) {
+                extendRect(rect, true);
+                g.setColor(color);
+                g.fillRect(rect.x, rect.y, rect.width, rect.height);
+            }
+            if (!loc.isInsertColumn() && shortColor != null) {
+                g.setColor(shortColor);
+                g.fillRect(x, rect.y, w, rect.height);
+            }
+        }
+
+        rect = getVDropLineRect(loc);
+        if (rect != null) {
+            int y = rect.y;
+            int h = rect.height;
+            if (color != null) {
+                extendRect(rect, false);
+                g.setColor(color);
+                g.fillRect(rect.x, rect.y, rect.width, rect.height);
+            }
+            if (!loc.isInsertRow() && shortColor != null) {
+                g.setColor(shortColor);
+                g.fillRect(rect.x, y, rect.width, h);
+            }
+        }
+    }
+
+    private Rectangle getHDropLineRect(JTable.DropLocation loc) {
+        if (!loc.isInsertRow()) {
+            return null;
+        }
+
+        int row = loc.getRow();
+        int col = loc.getColumn();
+        if (col >= table.getColumnCount()) {
+            col--;
+        }
+
+        Rectangle rect = table.getCellRect(row, col, true);
+
+        if (row >= table.getRowCount()) {
+            row--;
+            Rectangle prevRect = table.getCellRect(row, col, true);
+            rect.y = prevRect.y + prevRect.height;
+        }
+
+        if (rect.y == 0) {
+            rect.y = -1;
+        } else {
+            rect.y -= 2;
+        }
+
+        rect.height = 3;
+
+        return rect;
+    }
+
+    private Rectangle getVDropLineRect(JTable.DropLocation loc) {
+        if (!loc.isInsertColumn()) {
+            return null;
+        }
+
+        boolean ltr = table.getComponentOrientation().isLeftToRight();
+        int col = loc.getColumn();
+        Rectangle rect = table.getCellRect(loc.getRow(), col, true);
+
+        if (col >= table.getColumnCount()) {
+            col--;
+            rect = table.getCellRect(loc.getRow(), col, true);
+            if (ltr) {
+                rect.x = rect.x + rect.width;
+            }
+        } else if (!ltr) {
+            rect.x = rect.x + rect.width;
+        }
+
+        if (rect.x == 0) {
+            rect.x = -1;
+        } else {
+            rect.x -= 2;
+        }
+
+        rect.width = 3;
+
+        return rect;
+    }
+
+    private Rectangle extendRect(Rectangle rect, boolean horizontal) {
+        if (rect == null) {
+            return rect;
+        }
+
+        if (horizontal) {
+            rect.x = 0;
+            rect.width = table.getWidth();
+        } else {
+            rect.y = 0;
+
+            if (table.getRowCount() != 0) {
+                Rectangle lastRect = table.getCellRect(table.getRowCount() - 1, 0, true);
+                rect.height = lastRect.y + lastRect.height;
+            } else {
+                rect.height = table.getHeight();
+            }
+        }
+
+        return rect;
+    }
+    
     public void paintViewport(Graphics g, JViewport c) {
         Dimension vs = c.getSize();
         Dimension ts = table.getSize();
